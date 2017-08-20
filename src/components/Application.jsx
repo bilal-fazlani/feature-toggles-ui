6e76121011affcd97f6c41335a51036e76038b53
bilal-fazlani <bilal.m.fazlani@gmail.com>
2017-08-20 08:59:43 +0530

    Removed collapsible styles

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,24 +1,15 @@
-import Collapsible from 'react-collapsible';
 import * as React from "react";
 import Feature from './Feature';
-import './Application.css'
 import * as PropTypes from 'prop-types'
 
 export default class Application extends React.Component{
 
     render(){
-        return <div className="collapsible-wrapper">
-            <Collapsible
-                easing="ease-in-out"
-                transitionTime={400} trigger={
-                <span className="collapsible-title">{this.props.applicationName}</span>}>
-                <div className="collapsible-content">
-                    <Feature featureName="new feature 0" toggles={{dev: false, qa: false, prod: false}} />
-                    <Feature featureName="new feature 1" toggles={{dev: true, qa: false, prod: false}} />
-                    <Feature featureName="new feature 2 " toggles={{dev: true, qa: true, prod: false}} />
-                    <Feature featureName="new feature 3" toggles={{dev: true, qa: true, prod: true}} />
-                </div>
-            </Collapsible>
+        return <div>
+            <Feature featureName="new feature 0" toggles={{dev: false, qa: false, prod: false}} />
+            <Feature featureName="new feature 1" toggles={{dev: true, qa: false, prod: false}} />
+            <Feature featureName="new feature 2 " toggles={{dev: true, qa: true, prod: false}} />
+            <Feature featureName="new feature 3" toggles={{dev: true, qa: true, prod: true}} />
         </div>
     }
 }
